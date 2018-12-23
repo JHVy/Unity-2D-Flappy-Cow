34a265c15323a69c8301a4b670431828e2391533
JHVy <39977095+JHVy@users.noreply.github.com>
2018-12-23 12:41:57 +0700

    23.12.18
    23.12.2018 Hoàn thành các thành cơ bản của game như : animations, collisions, sounds, sprite của các objects (cow, pipe, spider, ground)

---
+++ Flappy Cow/Assets/Scripts/BGScaler/BGScaler.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BGScaler : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        SpriteRenderer sr = GetComponent<SpriteRenderer>();
+        Vector3 tempScale = transform.localScale;
+
+        float height = sr.bounds.size.y;
+        float width = sr.bounds.size.x;
+
+        float worldHeight = Camera.main.orthographicSize * 2f; // 10
+        float worldWidth = worldHeight * Screen.width / Screen.height; // 10*203/339
+        //transform.localScale = new Vector3(worldWidth, worldHeight, 0);
+
+        tempScale.y = worldHeight / height;
+        tempScale.x = worldWidth / width;
+
+        transform.localScale = tempScale;
+    }
+}
---
+++ Flappy Cow/Assets/Scripts/PipeController/PipeHolder.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PipeHolder : MonoBehaviour
+{
+
+    public float speed;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(CowController.instance != null)
+        {
+            if(CowController.instance.flag == 1)
+            {
+                Destroy(GetComponent<PipeHolder>());
+            }
+        }
+        _PipeMovement();
+    }
+
+    void _PipeMovement()
+    {
+        Vector3 temp = transform.position;
+        temp.x -= speed * Time.deltaTime; // * detalTime cho game muot hon
+        transform.position = temp;
+    }
+
+    void OnCollisionEnter2D(Collision2D target)
+    {
+
+    }
+
+    void OnTriggerEnter2D(Collider2D target)
+    {
+        if(target.tag == "Destroy")
+        {
+            Destroy(gameObject);
+        }
+    }
+}
