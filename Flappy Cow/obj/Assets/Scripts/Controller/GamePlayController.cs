fb53703d6da7a3910d11441baf84b9cc46499480
JHVy <39977095+JHVy@users.noreply.github.com>
2018-12-25 19:31:40 +0700

    Hoàn thành game cơ bản
    Hoàn thành game cơ bản, đang bổ sung chức năng show điểm và huy chương khi ng chơi kết thúc game

---
+++ Flappy Cow/obj/Assets/Scripts/Controller/GamePlayController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GamePlayController : MonoBehaviour
+{
+    [SerializeField]
+    private Button instructionButton;
+
+    void Awake()
+    {
+        Time.timeScale = 0;
+    }
+
+    public void _InstructionButton()
+    {
+        Time.timeScale = 1;
+        instructionButton.gameObject.SetActive(false);
+    }
+}
